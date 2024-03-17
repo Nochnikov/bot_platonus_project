c05d7e8477ac6e62525ef0690d3d570c410bf2dc
Nurdaulet <dantesnurdaulet@gmail.com>
2024-03-17 16:17:53 +0600

    Added opportunity to get week days from schedules

--- main_selenium.py
+++ main_selenium.py
@@ -5,81 +5,53 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from os import getenv
 from dotenv import load_dotenv
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+main_url = 'https://edu2.aues.kz/index?sid=043175e87722aaff702e22fdeeb6eb61&returnUrl=%2Fv7%2F%23%2Fschedule%2FstudentView'
 
 
-def clicker_cather(driver: webdriver.Chrome, value):
-    try:
-        value.click()
-    except selenium.common.exceptions.StaleElementReferenceException:
-        pass
-    finally:
-        new_url = driver.current_url
-        return new_url
+def login_user(login: str, password: str):
+    driver = webdriver.Chrome(keep_alive=False)
 
-
-def get_source(driver: webdriver.Chrome, url: str):
-    driver.get(url)
-    source = driver.page_source
-    return source
-
-
-def get_map_site(driver: webdriver.Chrome, url: str):
-    driver.get(url)
-    map_site = driver.find_element(By.LINK_TEXT, 'Карта сайта')
-    new_url = clicker_cather(driver, value=map_site)
-    return new_url
-
-
-def map_menu_choice(user_choice, driver: webdriver.Chrome, url: str):
-    driver.get(url)
-    value_url = ''
-
-    if user_choice == 'Расписание':
-        value = driver.find_element(By.LINK_TEXT, f'{user_choice}')
-        value_url = clicker_cather(driver, value)
-    return value_url
-
-
-def login(url: str, login: str, password: str):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    assert 'Platonus' in driver.title
-    log = driver.find_element(By.NAME, 'login')
-    pword = driver.find_element(By.ID, 'pass_input')
-    submit = driver.find_element(By.ID, 'Submit1')
-    log.clear()
-    pword.clear()
+    driver.get(main_url)
 
     try:
-        log.send_keys(login + Keys.RETURN)
-        pword.send_keys(password + Keys.RETURN)
-        submit.click()
+        login_field = driver.find_element(By.NAME, 'login')
+        login_field.send_keys(login + Keys.RETURN)
+
+        password_field = driver.find_element(By.ID, 'pass_input')
+        password_field.send_keys(password + Keys.RETURN)
+
+        login_button = driver.find_element(By.ID, 'Submit1')
+        login_button.click()
+
     except selenium.common.exceptions.StaleElementReferenceException:
         pass
-    finally:
-        new_url = driver.current_url
 
-    map_url = get_map_site(driver, new_url)
+    wait = WebDriverWait(driver, 10)  # Ожидание до 10 секунд
+    schedule_element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'card-body')))
 
-    menu_item = map_menu_choice('Расписание', driver=driver, url=map_url)
+    page_content = driver.page_source
 
-    chosen_item_source = get_source(driver, menu_item)
+    driver.quit()
 
-    driver.close()
-
-    return chosen_item_source
+    return page_content
 
 
 if __name__ == '__main__':
     load_dotenv()
-    url = 'https://edu2.aues.kz'
-    username = getenv('LOGIN')
-    password = getenv('password')
 
-    html_code = login(url, username, password)
+    log = getenv('LOGIN')
 
-    soup = BeautifulSoup(html_code, 'html.parser')
+    password = getenv('PASSWORD')
+
+    values = login_user(log, password)
+    # print(values)
+
+    soup = BeautifulSoup(values, 'html.parser')
 
     weekdays = soup.find_all(name='h5', class_='card-title')
 
-    print(html_code)
+
+    print(weekdays)
